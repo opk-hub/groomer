ac80825e45985a90edc66d07afe81b18f1b9cf93
praveenkumaronamala <126156412+opk-hub@users.noreply.github.com>
2024-06-08 01:59:53 +0530

    Add files via upload
    first commit

---
+++ src/Context/Context.js
@@ -0,0 +1,21 @@
+const isLocal = false;
+
+const Context = isLocal
+  ? "http://127.0.0.1:8000"
+  : "https://groomerapp-e1f70ad38863.herokuapp.com";
+
+const SALON_TOKEN = "salon_token";
+
+export const setToken = (token) => {
+  sessionStorage.setItem(SALON_TOKEN, token);
+};
+
+export const removeToken = () => {
+  sessionStorage.removeItem(SALON_TOKEN);
+};
+
+export const getToken = () => {
+  let token = sessionStorage.getItem(SALON_TOKEN);
+  return token;
+};
+export default Context;
